51fdbf38770893b37e5c4fa512bb8749eb1226e7
sher0201 <43050369+sher0201@users.noreply.github.com>
2019-08-28 16:12:52 -0400

    one more way to solve

--- tdRefresh/main.js
+++ tdRefresh/main.js
@@ -6,7 +6,30 @@ Some of the data need to be refreshed every 5 seconds. Without reloading the who
 
 //First idea: AJAX + RegExp in JavaScript
 
+//Refresh the whole body
+function refreshBody(){
+  setInterval(function refreshCells(){
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        let str=this.responseText
+        let regexpResult=str.match(/<body.*>([^]*)<\/body>/)[1];
+        /*
+        [^] - any simbol
+        ([^]*) - The parentheses indicate that I want to capture/extract this portion of the regex,
+        while ignoring the rest of it.
+        Usually goes as match [1] element of the array
+        */
+        document.getElementsByTagName("body")[0].innerHTML= regexpResult;
+      }
+    };
+      //just get the page content I am at
+    xhttp.open("GET", "sensors.cgi", true);
+    xhttp.send();
+  },10000);
+}
 
+//refresh one cell
 function refreshCell(){
 //set interval to refresh page every 5 sec (,5000)
     setInterval(function refreshCells(){
@@ -17,16 +40,11 @@ function refreshCell(){
           var str=this.responseText
           //search for specific id - refresh and get content of the cell
           var a = str.match(/<td.*refresh[^>]+>([^<]+)</)[1];
-          /*
-          ([^<]+) - The parentheses indicate that I want to capture/extract this portion of the regex,
-          while ignoring the rest of it.
-          Usually goes as match [1] element of the array
-          */
           document.getElementById("refresh").innerHTML = a;
         }
       };
       //just get the page content I am at
-      xhttp.open("GET", "/cgi/sensors.cgi", true);
+      xhttp.open("GET", "sensors.cgi", true);
       xhttp.send();
     },5000);
 
